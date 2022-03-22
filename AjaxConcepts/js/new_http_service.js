d5f67305016651f2249b662cddf04de2d4bdb58e
basha <bashamb787@gmail.com>
2022-03-22 12:07:22 +0530

    Ajax Concepts

--- AjaxConcepts/js/new_http_service.js
+++ AjaxConcepts/js/new_http_service.js
@@ -1,13 +1,12 @@
 
-
 function makePromiseCall(methodType, url, async = true, data = null) {
     return new Promise(function (resolve, reject) {
     let xhr = new XMLHttpRequest();
     xhr.onload = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200 || xhr.status === 201) {
+      console.log(methodType+ "state changed called Ready State" +xhr.readyState+"status" +xhr.status);
+      if (xhr.status.toString().match('^[2][0-9]{2}$')) {
           resolve(xhr.responseText);
-        } else if (xhr.status >= 400) {
+        } else if (xhr.status.toString().match('^[4,5][0-9]{2}$')) {
           reject({
             status: xhr.status,
             statusText: xhr.statusText,
@@ -16,11 +15,10 @@ function makePromiseCall(methodType, url, async = true, data = null) {
           console.log("Handle 400 Client Error or 500 Server Error.");
         }
       }
-    }
     xhr.onerror = function(){
         reject({
             status: xhr.status,
-            statusText: xhr.statusText,
+            statusText: xttp.statusText
           });
     };
     xhr.open(methodType, url, async);
@@ -32,35 +30,3 @@ function makePromiseCall(methodType, url, async = true, data = null) {
     console.log(methodType + " request sent to the server.");
   });
   }
-
-
-
-//   function makePromiseCall(methodType, url, async = true, data = null) {
-//     return new Promise(function (resolve, reject) {
-//       let xhr = new XMLHttpRequest();
-//       xhr.onreadystatechange = function () {
-//         console.log(
-//           "State Changed Called. Ready State: " +
-//             xhr.readyState +
-//             " Status: " +
-//             xhr.status
-//         );
-//         if (xhr.status.toString().match("^[2][0-9]{2}$")) {
-//           resolve(xhr.responseText);
-//         } else if (xhr.status.toString().match("^[4,5][0-9]{2}$")) {
-//           reject({
-//             status: xhr.status,
-//             statusText: xhr.statusText,
-//           });
-//           console.log("XR Failed!");
-//         }
-//       };
-//       xhr.open(methodType, url, async);
-//       if (data) {
-//         console.log(JSON.stringify(data));
-//         xhr.setRequestHeader("Content-Type", "application/json");
-//         xhr.send(JSON.stringify(data));
-//       } else xhr.send();
-//       console.log(methodType + " request sent to the server.");
-//     });
-//   }
